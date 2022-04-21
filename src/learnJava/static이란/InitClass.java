2fe6ea9e3d59275b65a24acb5297c469d65678d6
seheonCho <whtpgjs94@gmail.com>
2022-04-21 19:16:06 +0900

    feat: static 예제
    1. InitClass.java
    2. LearnStatic.java

---
+++ src/learnJava/static이란/InitClass.java
@@ -0,0 +1,20 @@
+package learnJava.static이란;
+
+public class InitClass {
+
+    static final int TEST_NUMBER;
+
+    static {
+        System.out.println("start static...");
+    }
+
+    static {
+        TEST_NUMBER = 100;
+        System.out.println(TEST_NUMBER);
+    }
+
+    static {
+        System.out.println("end static..");
+    }
+
+}
---
+++ src/learnJava/static이란/LearnStatic.java
@@ -0,0 +1,9 @@
+package learnJava.static이란;
+
+public class LearnStatic {
+
+    public static void main(String[] args) {
+        System.out.println(InitClass.TEST_NUMBER);
+    }
+
+}
