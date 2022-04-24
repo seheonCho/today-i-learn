fe5ebfbd70e5e4a2883dbc39e7de096d4dfe7070
seheonCho <whtpgjs94@gmail.com>
2022-04-24 11:48:41 +0900

    feat: 실수 자료형 예제
    LearnRealNumberType.java

---
+++ src/learnJava/숫자관련/실수자료형/LearnRealNumberType.java
@@ -0,0 +1,20 @@
+package learnJava.숫자관련.실수자료형;
+
+public class LearnRealNumberType {
+
+    public static void main(String[] args) {
+
+        float aFloatNum = 1.12f;
+        double aDoubleNum = 1.12;
+
+        System.out.println(aFloatNum);
+        System.out.println(aDoubleNum);
+
+        aFloatNum += 0.12;
+        aDoubleNum += 0.12;
+
+        System.out.println(aFloatNum);
+        System.out.println(aDoubleNum);
+    }
+
+}
